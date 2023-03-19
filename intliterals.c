9132e90d37d1b6f865907046ac243ea5832de92e
AliZainoul <ali.zainoul.az@gmail.com>
2023-03-19 18:35:06 +0100

    Modifying file intliterals.c

--- intliterals.c
+++ intliterals.c
@@ -1,3 +1,9 @@
+/*  In order to compile this program:
+  - Open a command prompt
+  - Type in:
+  $  gcc -o i intliterals.c && ./i
+*/
+
 #include <stdio.h>
 
 void draw_line() { printf("------------- \n"); }
@@ -5,37 +11,27 @@ void draw_line() { printf("------------- \n"); }
 int main()
 {
 
-  int a = 212;         /* Legal and prints simply 212 */
-  int b = 215u;        /* Legal and prints 215 as unsigned integer */
+  int a = 212;         /* Decimal: int */
+  /* Legal and prints simply 212 */
+  int b = 215u;        /* Decimal: int */
+  /* Legal and prints 215 as unsigned integer */
   int c = 0xFeeL;
   /* Legal and prints 4078 as long (hexadecimal to decimal)
   (FEE)₁₆ = (15 × 16²) + (14 × 16¹) + (14 × 16⁰) = (4078)₁₀
   */
-  int  d = 85;          /* decimal */
-  int  e = 0x4b;        /* hexadecimal */
+  int  d = 85;          /* Decimal: int */
+  /* Decimal number, prints it as it is. */
+  int  e = 0x4b;        /* Hexadecimal: int */
   /* Legal and prints 4078 as long (hexadecimal to decimal)
   (4B)₁₆ = (4 × 16¹) + (11 × 16⁰) = (75)₁₀
   */
-  int  f = 30;          /* int */
-  int  g = 30u;         /* unsigned int */
-  int  h = 30l;         /* long */
-  int  i = 30ul;        /* unsigned long */
+  int  f = 30;          /* Decimal: int */
+  int  g = 30u;         /* Decimal: unsigned int */
+  int  h = 30l;         /* Decimal: long */
+  int  i = 30ul;        /* Decimal: unsigned long */
 
   //int d = 078         /* Illegal: 8 is not an octal digit */
   //int e = 032UU       /* Illegal: cannot repeat a suffix */
-
-
-
-  // auto int j = {i};
-  int count = 10;
-  int & countRef = count;
-  //auto myAuto = countRef;
-
-  //countRef = 11;
-  printf("%d \n", count);
-
-  // myAuto = 12;
-  printf("%d \n", count);
 
   draw_line();
   printf ("a is : %d \n", a);
